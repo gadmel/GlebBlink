377a5123f1de507e80f154560c13a0835d5cbc09
Gleb Abramov <abramov.gleb@gmail.com>
2020-07-15 13:22:23 +0200

    Connect an rgb-diode, anode to gnd 200ohm resistor

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,37 @@
 #include <Arduino.h>
-#define onboard 13
+#define led_onboard 13
+#define led_1_R 5
+#define led_1_G 6
+#define led_1_B 3
 
 int a = 1;
 void setup()
 {
-  pinMode(onboard, OUTPUT);
+  pinMode(led_onboard, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  digitalWrite(onboard, LOW);
-  delay(3000);
-  digitalWrite(onboard, HIGH);
+  digitalWrite(led_onboard, HIGH);
+  delay(100);
+  digitalWrite(led_onboard, LOW);
+  delay(100);
+  digitalWrite(led_1_R, HIGH);
   delay(1000);
-  Serial.print("Loop completed ");
+  digitalWrite(led_1_R, LOW);
+  digitalWrite(led_1_G, HIGH);
+  delay(1000);
+  digitalWrite(led_1_G, LOW);
+  digitalWrite(led_1_B, HIGH);
+  delay(1000);
+  digitalWrite(led_1_B, LOW);
+  delay(1000);
+  digitalWrite(led_onboard, HIGH);
+  delay(100);
+  digitalWrite(led_onboard, LOW);
+  delay(100);
+  Serial.print("RGB-Loop completed ");
   Serial.print(a);
   Serial.println(" times.");
   ++a;
